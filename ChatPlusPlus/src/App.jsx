f74856e58c8327410fc7d34a3dc9635dec14ab53
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2025-07-11 00:30:14 -0700

    formatted files

--- ChatPlusPlus/src/App.jsx
+++ ChatPlusPlus/src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
 import RequireAuth from "./features/auth/RequireAuth";
 import LoginPage from "./pages/auth/LoginPage";
 import RegisterPage from "./pages/auth/RegisterPage";
@@ -8,17 +13,23 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/login" element={ <LoginPage /> }></Route>
-                <Route path="/register" element={ <RegisterPage /> }></Route>
-                <Route path="/chats" element={ 
-                    <RequireAuth>
-                        <ChatPage /> 
-                    </RequireAuth>
-                }/>
-                <Route path="*" element={ <Navigate to="/login" replace /> }></Route>
+                <Route path="/login" element={<LoginPage />}></Route>
+                <Route path="/register" element={<RegisterPage />}></Route>
+                <Route
+                    path="/chats"
+                    element={
+                        <RequireAuth>
+                            <ChatPage />
+                        </RequireAuth>
+                    }
+                />
+                <Route
+                    path="*"
+                    element={<Navigate to="/login" replace />}
+                ></Route>
             </Routes>
         </Router>
     );
 }
 
-export default App
+export default App;
